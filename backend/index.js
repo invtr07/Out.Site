576348b8c7a029a7cf0dd8a72168da370f169210
effuone <effuone>
2022-07-24 19:36:25 +0600

    app side separation & fix

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv/config'
+
+const PORT = process.env.PORT ||  5000
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const start = async () => {
+     try{
+         await mongoose.connect(process.env.OUTSITE_DB_URI)
+         app.listen(PORT, ()=> {
+             console.log(`listening on port ${PORT}`)
+         })
+     } catch(e) {
+         console.log(e)
+     }
+ }
+ 
+await start()
